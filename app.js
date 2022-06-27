75892c4d36ad5d67022561b68316e85f092780a0
D Krishnakanth <krishna699@Ds-MacBook-Air.local>
2022-06-27 16:18:56 +0530

    Level - 3 Hashing

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require('mongoose-encryption');
-
+// const encrypt = require('mongoose-encryption');
+const md5 = require("md5");
 
 const app = express();
 
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ["password"]});
+// userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ["password"]});
 
 const User = mongoose.model("User",userSchema);
 
@@ -41,7 +41,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -59,7 +59,7 @@ app.post("/login",function(req,res){
         if(err){
             console.log(err);
         }else{
-            if(results && (results.password === req.body.password)){
+            if(results && (results.password === md5(req.body.password))){
                 res.render("secrets");
             }
         }
