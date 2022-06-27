fdeb4f2ebf421fe1a0176f2cab33af63fe7a5ed7
D Krishnakanth <krishna699@Ds-MacBook-Air.local>
2022-06-27 13:54:18 +0530

    Adding environment variables

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -6,6 +7,8 @@ const encrypt = require('mongoose-encryption');
 
 
 const app = express();
+
+console.log(process.env.API_KEY);
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
@@ -19,8 +22,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "Thisismylittlesecret.";
-userSchema.plugin(encrypt, { secret: secret , encryptedFields: ["password"]});
+userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ["password"]});
 
 const User = mongoose.model("User",userSchema);
 
